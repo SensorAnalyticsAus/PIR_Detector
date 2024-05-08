9d20c81101bc9ce7484534a447e331e4ecbe7f5c
SensorAnalyticsAus <114129496+SensorAnalyticsAus@users.noreply.github.com>
2024-05-08 21:30:40 +1000

    new update

--- pir.py
+++ pir.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
 
 ###############################################################################
-#         HC-SR501 PIR MOTION DETECTOR Sensor Code for RaspberryPi 
-#                      Sensor Analytics™ Australia 2024
+#         HC-SR501 PIR MOTION DETECTOR Sensor Code for RaspbberryPi 
+#                       ©Sensor Analytics Australia 2024
 ############################################################################### 
 
-Mute = 0     # 0 - mute, 1 - enable pushover.net sound alert and alarm 
-po_token='your_pushover_app_token' # optional
-po_user='your_pushover_user_code'  # optional
+Mute = 1     # 0 - mute, 1 - enable pushover.net sound alert and alarm 
+sound_alert='tugboat'
+po_token='insert your pushover app token here'  # optional
+po_user='insert your pushover user key here'    # optional
 TolAlert = 3 # three successive md events
 TolAlarm = 7 # seven successive md events
 pin = 40     #physical pin 40 (not gpio no etc) connected to pir's signal wire
+LOGFILE = '/mnt/32G/log/PIR.log' # change this to your preferred location
 
 import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 import signal,sys
 import json,http.client, urllib #for po
+import os
 
 def po(msg,snd): #pushover PIR_MD to send msg to mobile, change token and user
  conn = http.client.HTTPSConnection("api.pushover.net:443")
@@ -34,10 +37,23 @@ def po(msg,snd): #pushover PIR_MD to send msg to mobile, change token and user
 def signal_handler(signal, frame):
  print('\ngotta go...')
  sys.exit(0)
+def makeDir(dirpath):
+ if not os.path.exists(dirpath):
+     os.makedirs(dirpath)
+     print(dirpath," created")
+ else:
+     print(dirpath," exists all good")
+def writeLog(msg,log_file):
+ with open(log_file,'a') as logfile:
+     logfile.write(msg+'\n')
+ return
 
 signal.signal(signal.SIGINT, signal_handler)
 print(datetime.now().strftime('%H:%M:%S'),'PIR Motion Detector started...')
 print('ctrl-C to exit')
+
+log_dir = os.path.dirname(LOGFILE)
+makeDir(log_dir)
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -49,14 +65,17 @@ while True:
     if i==1:              
         knt+=1
         print (now,'Motion Detected',i)
+        if LOGFILE: writeLog(now+' Motion Detected '+str(i),LOGFILE)
         if knt == TolAlert: 
             msg=now+' Motion Detect Alert'
             print(msg)
-            if Mute == 1: po(msg,'gamelan')
+            if LOGFILE: writeLog(msg,LOGFILE)
+            if Mute == 1: po(msg,sound_alert)
         if knt == TolAlarm: 
             msg=now+' Intruder Alarm Check Cameras!'
             print(msg)
-            if Mute == 1: po(msg,'siren')
+            if LOGFILE: writeLog(msg,LOGFILE)
+            if Mute == 1: po(msg,'alarm')
         time.sleep(0.1)
     else: knt=0 # reset counter
     time.sleep(.5)
